bee1c84159e61aa7b8f8d17653cb0f8bcca8b2e3
Paulo Henrique <contato@paulohenriquesn.com>
2023-06-24 13:38:18 -0300

    feat: add extract observer

---
+++ src/observers/extract.observer.ts
@@ -0,0 +1,12 @@
+import { ACTION } from "../domain/enums/actions.enum";
+import { Observer } from "../domain/protocols/observer.protocol";
+import { makeExtract } from "../factories/account/extract.factory";
+
+export class ExtractObserver implements Observer {
+  async next({ action, document }: { action: ACTION, document: string }) {
+    if (action !== ACTION.EXTRACT) return;
+    await makeExtract().execute({
+      document,
+    })
+  }
+}
