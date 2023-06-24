e18a294e72f68a084accba8f36dc61ae12ae140c
Paulo Henrique <contato@paulohenriquesn.com>
2023-06-24 13:38:29 -0300

    feat: add withdraw observer

---
+++ src/observers/withdraw.observer.ts
@@ -0,0 +1,13 @@
+import { ACTION } from "../domain/enums/actions.enum";
+import { Observer } from "../domain/protocols/observer.protocol";
+import { makeWithdraw } from "../factories/account/withdraw.factory";
+
+export class WithdrawObserver implements Observer {
+  async next({ action, document, amount }: { action: ACTION, document: string, amount: number }) {
+    if (action !== ACTION.WITHDRAW) return;
+    console.log(await makeWithdraw().execute({
+      document,
+      amount
+    }))
+  }
+}
