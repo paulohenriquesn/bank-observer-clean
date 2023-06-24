bf91b8051c2aaf0fd691728d0b2316c0931d5efc
Paulo Henrique <contato@paulohenriquesn.com>
2023-06-24 13:38:58 -0300

    feat: add all observers as listener for observable

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import data from './data.json'
+import { Observer } from './domain/protocols/observer.protocol';
+import { AccountObservable } from './observables/account.observable';
+import { CreateAccountObserver } from './observers/create-account.observer';
+import { DepositObserver } from './observers/deposit.observer';
+import { ExtractObserver } from './observers/extract.observer';
+import { WithdrawObserver } from './observers/withdraw.observer';
+
+const observers: Record<string, Observer> = {
+  create_account: new CreateAccountObserver(),
+  withdraw: new WithdrawObserver(),
+  deposit: new DepositObserver(),
+  extract: new ExtractObserver()
+}
+
+const observable = new AccountObservable()
+
+Object.keys(observers).forEach((observableKey: string) => {
+  observable.addObserver(observers[observableKey])
+})
+
+data.forEach((event) => {
+  observable.emit(event);
+})
